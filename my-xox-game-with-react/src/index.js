0b9366cdad24245ef0dc40bae92fb53d4d63b1a1
Sena Sümer <ssena6050@gmail.com>
2025-07-10 09:54:14 +0300

    first commit

---
+++ my-xox-game-with-react/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+
+function XoxGameComponent(){
+  return(
+    <h1>XOX Game</h1>
+  )
+}
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+<XoxGameComponent/>
+);
+
+
+reportWebVitals();
